315f982a1247b23ae6ff77649a6cd1d2787b78c5
test <test@example.com>
2024-07-17 15:20:41 +0900

    up

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
-var app = express();
+const startUpTime = dateformat();
+let accessCount = 0;
+const app = express();
 const port = process.env.PORT || 3000
 const server = app.listen(port);
 app.get("/", function(req, res, next){
+  accessCount+=1;
   console.log(`アクセスがありました`);
-    res.json({status:"動作しています"});
+    res.json({status:"動作しています",accessCount,startUpTime,now:dateformat(),});
 });
+
+
+function dateformat(dateObj = new Date(), format = "YYYY-MM-DD HH:mm:ss.SSS") {
+  let result = format;
+  result = result.replace(/YYYY/g, dateObj.getFullYear().toString());
+  result = result.replace(/MM/g, (dateObj.getMonth() + 1).toString().padStart(2, "0"));
+  // result = result.replace(/DDD/g, ["日", "月", "火", "水", "木", "金", "土"][dateObj.getDay()]);
+  result = result.replace(/DDD/g, ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][dateObj.getDay()]);
+  result = result.replace(/DD/g, dateObj.getDate().toString().padStart(2, "0"));
+  result = result.replace(/HH/g, dateObj.getHours().toString().padStart(2, "0"));
+  result = result.replace(/mm/g, dateObj.getMinutes().toString().padStart(2, "0"));
+  result = result.replace(/ss/g, dateObj.getSeconds().toString().padStart(2, "0"));
+  result = result.replace(/SSS/g, dateObj.getMilliseconds().toString().padStart(3, "0"));
+  return result;
+}
--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-var app = express();
-const port = process.env.PORT || 3000
-const server = app.listen(port);
-app.get("/", function(req, res, next){
-  console.log(`アクセスがありました`);
-    res.json({status:"動作しています"});
-});
